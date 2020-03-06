ed20d36b1ef355edf5b28a98a85f155a2d729a17
LarissaBruebach <larissa.bruebach@stud-mail.uni-wuerzburg.de>
2020-03-06 14:55:08 +0000

    fixed walking

--- Maze/Assets/MazeTask/Scripts/VRController.cs
+++ Maze/Assets/MazeTask/Scripts/VRController.cs
@@ -5,8 +5,7 @@ using Valve.VR;
 
 public class VRController : MonoBehaviour {
 
-    public float m_Sensitivity = 0.1f;
-    public float m_MaxSpeed = 1.0f;
+    public float m_Sensitivity = 0.2f;
 
     public SteamVR_Action_Boolean m_MovePress = null;
     public SteamVR_Action_Vector2 m_MoveValue = null;
@@ -45,6 +44,7 @@ public class VRController : MonoBehaviour {
         // Restore
         m_CameraRig.position = oldPosition;
         m_CameraRig.rotation = oldRotation;
+        
 
     }
 
@@ -64,12 +64,12 @@ public class VRController : MonoBehaviour {
         // if button pressed
         if (m_MovePress.state)
         {
-            // add, clamp
-            m_Speed = m_MoveValue.axis.y * m_Sensitivity;
-            m_Speed = Mathf.Clamp(m_Speed, -m_MaxSpeed, m_MaxSpeed);
+            // set speed
+            m_Speed = 2.0f;
 
             // orientation
             movement += orientation * (m_Speed * Vector3.forward) * Time.deltaTime;
+
         }
 
         // apply
