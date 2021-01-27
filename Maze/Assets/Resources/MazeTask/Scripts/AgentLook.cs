170a35e9cc85fa8283d92e33aadda077429fb160
Larissa Brübach <larissa.bruebach@uni-wuerzburg.de>
2021-01-27 19:13:54 +0100

    removed unnecessary lines

--- Maze/Assets/Resources/MazeTask/Scripts/AgentLook.cs
+++ Maze/Assets/Resources/MazeTask/Scripts/AgentLook.cs
@@ -20,23 +20,17 @@ public class AgentLook : MonoBehaviour
         {
             fakeEye.LookAt(eyeDest);
             Vector3 rot = fakeEye.localEulerAngles;
-            bool a = rot.x > 350;
-            bool b = rot.x < 10;
-            bool c = rot.y > 330;
-            bool d = rot.y < 30;
             if (Agent.Equals("A"))
             {
                 if ((rot.x > 350 || rot.x < 10) && (rot.y > 330 || rot.y < 30))
                 {
                     transform.LookAt(eyeDest);
-                    transform.Rotate(0, 90, 0);
                 } 
             } else if (Agent.Equals("B"))
             {
                 if ((rot.x > 350 || rot.x < 10) && (rot.y > 255 || rot.y < 305))
                 {
                     transform.LookAt(eyeDest);
-                    transform.Rotate(0, 90, 0);
                 }
             }
             
