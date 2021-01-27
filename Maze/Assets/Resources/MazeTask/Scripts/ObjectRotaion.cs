d97359fd89bc2b1faa5853a99f96d3a053a6b920
Larissa Brübach <larissa.bruebach@uni-wuerzburg.de>
2021-01-27 18:20:38 +0100

    Add scaling of KIs and fix desktop player

--- Maze/Assets/Resources/MazeTask/Scripts/ObjectRotaion.cs
+++ Maze/Assets/Resources/MazeTask/Scripts/ObjectRotaion.cs
@@ -7,6 +7,13 @@ public class ObjectRotaion : MonoBehaviour
     float speed = 0.2f;
     public bool shouldRotate = false;
 
+    public float min = 0.9f;
+    public float max = 1f;
+
+    public float sin = 0f;
+
+    public float speedSin = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +26,37 @@ public class ObjectRotaion : MonoBehaviour
         if(shouldRotate == true)
         {
             transform.Rotate(speed, speed, 0); //makes the Object rotate on the y and x axis with the value of speed
+
+            ScaleObject();
         }
-       
+
     }
+
+    private void ScaleObject()
+    {
+        sin = Mathf.Sin(Time.time * speedSin); // -1 - 1
+        sin = sin.Remap(-1f, 1f, min, max);
+        Vector3 vec = new Vector3(sin, sin, sin);
+
+        transform.localScale = vec;
+    }
+}
+
+public static class ExtensionMethods
+{
+    public static float Remap(this float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        var fromAbs = from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;
+
+        var normal = fromAbs / fromMaxAbs;
+
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
+
+        var to = toAbs + toMin;
+
+        return to;
+    }
+
 }
