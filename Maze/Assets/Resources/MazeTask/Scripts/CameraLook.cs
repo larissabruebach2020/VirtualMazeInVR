4cd039b0f00756d11af515ec8b6b98396be5265a
zcbtmfc <60511901+zcbtmfc@users.noreply.github.com>
2021-09-01 07:19:17 -0700

    Added new Scripts for Head Tracking

---
+++ Maze/Assets/Resources/MazeTask/Scripts/CameraLook.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Will rotate given objects to look in the direction of target gameObject ("CenterEyeAnchor" recommended for rift)
+// If the object is close enough, and within default angle.
+// Script is made for imported MMD models, and relies on the animation setting the default angle every frame for the object
+
+[System.Serializable]
+public class LookingEntry
+{
+    public GameObject item;
+    public float maximumAngle = 150f;
+    public float turnSpeed = 1f;
+    public float maximumRectionDistance = 30f;
+    [Range(0.1f, 1.0f)]
+    public float turnPercentage = 1f;
+}
+
+public class CameraLook : MonoBehaviour
+{
+    public GameObject target;
+    public LookingEntry[] parts;
+
+    Quaternion[] rotations;
+
+    // Use this for initialization
+
+    void Start()
+    {
+        rotations = new Quaternion[parts.Length];
+
+        for (int i = 0; i < parts.Length; i++)
+        {
+            rotations[i] = parts[i].item.transform.rotation;
+        }
+    }
+
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Follow");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        GameObject target = GameObject.FindGameObjectWithTag("Follow");
+        GameObject item = GameObject.FindGameObjectWithTag("Head");
+        LookingEntry entry;
+        for (int i = 0; i < parts.Length; i++)
+        {
+            entry = parts[i];
+            item = entry.item;
+            if (item != null)
+            {
+                Vector3 angle = (target.transform.position - item.transform.position).normalized;
+
+                Quaternion lookRotation;
+
+                if ((Vector3.Angle(item.transform.forward, angle) < entry.maximumAngle) && (Vector3.Distance(item.transform.position, target.transform.position) < entry.maximumRectionDistance))
+                {
+                    lookRotation = Quaternion.Slerp(item.transform.rotation, Quaternion.LookRotation(angle), entry.turnPercentage);
+                }
+                else
+                {
+                    lookRotation = item.transform.rotation;
+                }
+                rotations[i] = item.transform.rotation = Quaternion.Slerp(rotations[i], lookRotation, Time.deltaTime * entry.turnSpeed);
+            }
+        }
+    }
+}
---
+++ Maze/Assets/Resources/MazeTask/Scripts/HeadTracking.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HeadTracking : MonoBehaviour
+{
+    public Transform Target;
+    public float Radius = 10f;
+
+    List<PointofInterest> POIs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        POIs = FindObjectsOfType<PointofInterest>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform tracking = null;
+        foreach (PointofInterest poi in POIs)
+        {
+            Vector3 delta = poi.transform.position - transform.position;
+            if(delta.magnitude < Radius)
+            {
+                tracking = poi.transform;
+                break;
+            }
+        }
+        if(tracking != null)
+        {
+            Target.position = tracking.position;
+        }
+    }
+}
